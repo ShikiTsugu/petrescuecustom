08fe8ad919187a104e9983308a70fb4127a46d63
sebastien <sebastienzhang864@gmail.com>
2020-11-28 16:50:39 +0100

    Calcul et affiche Score

--- POOprojet/src/Score.java
+++ POOprojet/src/Score.java
@@ -1,2 +1,17 @@
 public class Score {
+	
+	int score;
+	
+	public Score() {
+		score = 0;
+	}
+	
+	public void calcul(int i){
+		score = score + i*i*10;
+	}
+	
+	public void affiche() {
+		System.out.println("Score = " + score);
+	}
+	
 }
