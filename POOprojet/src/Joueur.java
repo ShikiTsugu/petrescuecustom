89a0c8c7d377eb476cf7ce5fb17ceb7280acce4f
sebastien <sebastienzhang864@gmail.com>
2020-11-28 17:50:34 +0100

    verif de coordonées dans Joeur

--- POOprojet/src/Joueur.java
+++ POOprojet/src/Joueur.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Joueur {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public int[] Coord() {
+		String s = sc.next();
+		int[] coord = {-1,-1};
+		if (s.length() == 2) {
+			char c1 = s.charAt(0);
+			char c2 = s.charAt(1);
+			if (Character.isUpperCase(c1) && Character.isDigit(c2)) {
+				coord[0] = c1-64;
+				coord[1] = Character.getNumericValue(c2);
+				return coord;
+			} else {
+				coord[0] = -1;
+				coord[1] = -1;
+				System.out.println("Coordonées non valide");
+			}
+		} else {
+			System.out.println("Coordonées non valide");
+		}
+		return coord;
+	}
+	
 }
