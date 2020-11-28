b5a44a0ce1599b4cfd3a62936be2f64a1c381a18
HOU <wtfomg@live.fr>
2020-11-28 15:51:01 +0100

    classe plateau définie

--- POOprojet/src/Plateau.java
+++ POOprojet/src/Plateau.java
@@ -1,2 +1,24 @@
 public class Plateau {
+    private Cube[][] cubes;
+
+    public Plateau(Cube[][] c){
+        cubes = c;
+    }
+
+    public void affiche(){
+        for(Cube[] c : cubes){
+            for(Cube b : c){
+                if(b instanceof Animaux){
+                    System.out.print(" A ");
+                }
+                if(b instanceof Bloc){
+                    System.out.print(" "+Bloc.getColor()+" ");
+                }
+                if(b instanceof Obstacle){
+                    System.out.print(" X ");
+                }
+            }
+        }
+    }
+
 }
