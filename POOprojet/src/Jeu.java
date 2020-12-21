6c271f4d37e6cd269ef01fa201902358ad8f9fbe
HOU <wtfomg@live.fr>
2020-12-21 12:24:58 +0100

    test des nouvelles méthodes

--- POOprojet/src/Jeu.java
+++ POOprojet/src/Jeu.java
@@ -7,8 +7,11 @@ public class Jeu {
         Cube b4 = new Bloc(0,0,"B");
         Cube a = new Animaux(0,0);
         Cube o = new Obstacle(0,0);
-        Cube[][] cubes = {{b1,a,b1,a,b2},{b1,b1,b2,b2,b1},{b1,b1,b4,b1,o},{b3,b3,b2,o,o},{b4,b4,o,o,o}};
+        Cube[][] cubes = {{b1,a,null,a,b2},{b1,b1,null,b2,b1},{b1,b1,null,b1,o},{b3,b3,null,o,o},{b4,b4,o,o,o}};
         Plateau p = new Plateau(cubes);
+        p.affiche();
+        System.out.println(p.colVide(2));
+        p.miseAJour();
         p.affiche();
     }
 }
