29fb23870e4cc07443b983d5e07e4e2ee3f6b7ce
HOU <wtfomg@live.fr>
2020-12-21 15:05:00 +0100

    Création des niveaux

--- POOprojet/src/Jeu.java
+++ POOprojet/src/Jeu.java
@@ -1,19 +1,40 @@
+import java.util.ArrayList;
+
 public class Jeu {
+    private ArrayList<Niveaux> niveaux = new ArrayList<Niveaux>();
+    private Cube r = new Bloc(0,0,"R");
+    private Cube g = new Bloc(0,0,"G");
+    private Cube y = new Bloc(0,0,"Y");
+    private Cube b = new Bloc(0,0,"B");
+    private Cube a = new Animaux(0,0);
+    private Cube o = new Obstacle(0,0);
+
+    public Jeu(){
+        Cube[][] c1 = {{null,a,null,null,null,a,null},{g,g,r,r,r,y,y},{g,g,b,b,b,y,y},{r,r,b,b,b,g,g},{r,r,b,b,b,g,g}
+        ,{g,y,y,r,g,g,r},{g,y,y,r,g,g,r}};
+        Plateau p1 = new Plateau(c1);
+        Niveaux n1 = new Niveaux(p1);
+        Cube[][] c2 = {{null,g,a,g,null},{a,g,a,g,a},{r,y,a,y,r},{r,y,r,y,r},{y,g,r,g,y},{y,g,r,g,y},{g,y,r,y,g},{g,y,r,y,g}};
+        Plateau p2 = new Plateau(c2);
+        Niveaux n2 = new Niveaux(p2);
+        Cube[][] c3 = {{null,null,a,null,a,null,a},{null,null,g,y,b,y,b},{null,null,g,y,b,y,b},{null,null,y,b,g,y,g},
+                {b,b,y,b,g,y,g},{g,b,b,g,b,g,b},{y,b,b,g,b,g,b},{g,g,y,b,y,o,o},{b,b,y,b,y,o,o}};
+        Plateau p3 = new Plateau(c3);
+        Niveaux n3 = new Niveaux(p3);
+        niveaux.add(n1);
+        niveaux.add(n2);
+        niveaux.add(n3);
+    }
+
+    public ArrayList<Niveaux> getNiv(){
+        return niveaux;
+    }
 
     public static void main(String[] args){
-        Cube b1 = new Bloc(0,0,"R");
-        Cube b2 = new Bloc(0,0,"G");
-        Cube b3 = new Bloc(0,0,"Y");
-        Cube b4 = new Bloc(0,0,"B");
-        Cube a = new Animaux(0,0);
-        Cube o = new Obstacle(0,0);
-        Cube[][] cubes = {{b1,a,null,a,b2},{b1,b1,null,b2,b1},{b1,b1,null,b1,o},{b3,b3,null,o,o},{b4,b4,o,o,o}};
-        Plateau p = new Plateau(cubes);
-        p.affiche();
-        System.out.println(p.colVide(2));
-        p.miseAJour();
-        p.affiche();
-        Niveaux n1 = new Niveaux(p);
-        System.out.println(n1);
+        Jeu j = new Jeu();
+        for(Niveaux n : j.getNiv()){
+            n.getPlateau().affiche();
+            System.out.println(n);
+        }
     }
 }
