61798b71f05500bc7f057160d6d25dec9fd53eb5
HOU <wtfomg@live.fr>
2020-12-22 15:43:16 +0100

    Update

--- POOprojet/src/Animaux.java
+++ POOprojet/src/Animaux.java
@@ -1,13 +1,11 @@
 public class Animaux extends Cube{
 	
 	private String type;
-	private final int score;
 	
 	public Animaux(int x, int y) {
 		super.x = x;
 		super.y = y;
 		type = "A";
-		score = 1000;
 	}
 
 	public String toString() {
