80750862b4089a3b9199fa16c76ea95ebac80a2a
HOU <wtfomg@live.fr>
2020-12-23 14:01:05 +0100

    gestion des out of bounds

--- POOprojet/src/Joueur.java
+++ POOprojet/src/Joueur.java
@@ -9,20 +9,25 @@ public class Joueur {
 		System.out.println("Entrez vos coordonées :");
 		String s = sc.next();
 		int[] coord = {-1,-1};
-		if (s.length() == 2) {
-			char c1 = s.charAt(0);
-			char c2 = s.charAt(1);
-			if (Character.isUpperCase(c1) && Character.isDigit(c2)) {
-				coord[0] = c1-65;
-				coord[1] = Character.getNumericValue(c2)-1;
-				return coord;
+		try {
+			if (s.length() == 2) {
+				char c1 = s.charAt(0);
+				char c2 = s.charAt(1);
+				if (Character.isUpperCase(c1) && Character.isDigit(c2)) {
+					coord[0] = c1 - 65;
+					coord[1] = Character.getNumericValue(c2) - 1;
+					return coord;
+				} else {
+					coord[0] = -1;
+					coord[1] = -1;
+					System.out.println("Coordonées non valide");
+				}
 			} else {
-				coord[0] = -1;
-				coord[1] = -1;
 				System.out.println("Coordonées non valide");
 			}
-		} else {
+		}catch (ArrayIndexOutOfBoundsException e){
 			System.out.println("Coordonées non valide");
+			Coord();
 		}
 		return coord;
 	}
