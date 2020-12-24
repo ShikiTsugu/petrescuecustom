b7b137054e31cf46ed6de9909f73a4261f5e71f4
HOU <wtfomg@live.fr>
2020-12-24 15:56:40 +0100

    update visuel

--- POOprojet/src/IntGraphView.java
+++ POOprojet/src/IntGraphView.java
@@ -1,42 +1,48 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 
 public class IntGraphView extends JFrame {
     private ImagePane imagePane;
+    private JLabel titre = new JLabel();
     private JButton jouer,quitter;
     private IntGraphModel model;
 
     public IntGraphView(IntGraphModel m){
         model = m;
-        JPanel boutons = new JPanel();
-        boutons.setLayout(new GridLayout(2,1));
         setTitle("Pet Rescue Saga");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         jouer = new JButton("Jouer");
         quitter = new JButton("Quitter");
         quitter.addActionListener((ActionEvent e) -> System.exit(0));
-        boutons.add(jouer);
-        boutons.add(quitter);
-        Dimension bd = boutons.getPreferredSize();
+        titre.setText("Pet Rescue Saga");
+        titre.setFont(new Font("Monospaced",Font.BOLD,60));
+
         imagePane = new ImagePane();
-        Dimension d = imagePane.getSize();
-        imagePane.setLayout(new FlowLayout(FlowLayout.CENTER, 0, d.height/2-bd.height/2));
-        imagePane.addComponentListener(new ComponentAdapter() {
-            public void componentResized(ComponentEvent evt) {
-                JPanel c = (JPanel) evt.getSource();
-                c.setLayout(new java.awt.FlowLayout(FlowLayout.CENTER, 0, c.getSize().height / 2 - bd.height / 2));
-            }
-        });
-        imagePane.add(boutons);
+
+        BoxLayout boxlayout = new BoxLayout(imagePane, BoxLayout.Y_AXIS);
+        imagePane.setLayout(boxlayout);
+        titre.setAlignmentX(Component.CENTER_ALIGNMENT);
+        imagePane.add(Box.createRigidArea(new Dimension(0, 120)));
+        imagePane.add(titre);
+        jouer.setAlignmentX(Component.CENTER_ALIGNMENT);
+        imagePane.add(Box.createRigidArea(new Dimension(0, 120)));
+        imagePane.add(jouer);
+        quitter.setAlignmentX(Component.CENTER_ALIGNMENT);
+        imagePane.add(Box.createRigidArea(new Dimension(0, 20)));
+        imagePane.add(quitter);
+
         setContentPane(imagePane);
     }
 
     public class ImagePane extends JPanel{
         public ImagePane(){
             setPreferredSize(new Dimension(model.getImage().getWidth(), model.getImage().getHeight()));
+        }
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(model.getImage(),0,0,this);
         }
     }
 }
