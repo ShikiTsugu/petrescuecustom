9071196833d0c0412c4a1a96449c65225f9d7b51
HOU <wtfomg@live.fr>
2020-12-24 13:39:34 +0100

    Ajout choix version

--- POOprojet/src/Jeu.java
+++ POOprojet/src/Jeu.java
@@ -146,13 +146,19 @@ public class Jeu {
     }
 
     public static void main(String[] args){
-        System.out.println("\n" +
-                "  ___     _     ___                       ___                 \n" +
-                " | _ \\___| |_  | _ \\___ ___ __ _  _ ___  / __| __ _ __ _ __ _ \n" +
-                " |  _/ -_)  _| |   / -_|_-</ _| || / -_) \\__ \\/ _` / _` / _` |\n" +
-                " |_| \\___|\\__| |_|_\\___/__/\\__|\\_,_\\___| |___/\\__,_\\__, \\__,_|\n" +
-                "                                                   |___/      \n");
-        Jeu j = new Jeu();
-        j.jouer();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("A quelle version voulez vous jouer ? Terminal(T) ou Graphique(G) ?");
+        String s = sc.nextLine();
+        if(s.equals("T")) {
+            System.out.println("\n" +
+                    "  ___     _     ___                       ___                 \n" +
+                    " | _ \\___| |_  | _ \\___ ___ __ _  _ ___  / __| __ _ __ _ __ _ \n" +
+                    " |  _/ -_)  _| |   / -_|_-</ _| || / -_) \\__ \\/ _` / _` / _` |\n" +
+                    " |_| \\___|\\__| |_|_\\___/__/\\__|\\_,_\\___| |___/\\__,_\\__, \\__,_|\n" +
+                    "                                                   |___/      \n");
+            Jeu j = new Jeu();
+            j.jouer();
+        }else if(s.equals("G")){}
+        else System.out.println("Je n'ai pas compris");
     }
 }
