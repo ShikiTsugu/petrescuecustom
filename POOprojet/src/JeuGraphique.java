fb29e11af70d213710ac30da1124c6ae26d5bc19
HOU <wtfomg@live.fr>
2020-12-24 13:58:01 +0100

    Ajout méthode

--- POOprojet/src/JeuGraphique.java
+++ POOprojet/src/JeuGraphique.java
@@ -1,2 +1,17 @@
+import java.awt.*;
+import java.io.File;
+
 public class JeuGraphique {
+    public void jouer(){
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                File f = new File("background.jpg");
+                String s = f.getAbsolutePath();
+                IntGraphModel im = new IntGraphModel(s);
+                IntGraphView iv = new IntGraphView(im);
+                iv.pack();
+                iv.setVisible(true);
+            }
+        });
+    }
 }
