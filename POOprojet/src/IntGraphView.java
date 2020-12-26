fa09d9cb862639c867378098048efe4d8e7ac103
sebastien <sebastienzhang864@gmail.com>
2020-12-26 11:15:38 +0100

    test selecteur de niveau pour interface graphique

--- POOprojet/src/IntGraphView.java
+++ POOprojet/src/IntGraphView.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 public class IntGraphView extends JFrame {
     private ImagePane imagePane;
     private JLabel titre = new JLabel();
-    private JButton jouer,quitter;
+    private JButton jouer,quitter,niv1;
     private IntGraphModel model;
 
     public IntGraphView(IntGraphModel m){
@@ -14,10 +14,13 @@ public class IntGraphView extends JFrame {
         setTitle("Pet Rescue Saga");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         jouer = new JButton("Jouer");
+        jouer.addActionListener((ActionEvent e) -> jouer());
         quitter = new JButton("Quitter");
-        quitter.addActionListener((ActionEvent e) -> System.exit(0));
+        quitter.addActionListener((ActionEvent e) -> affichemenu());
         titre.setText("Pet Rescue Saga");
         titre.setFont(new Font("Monospaced",Font.BOLD,60));
+        niv1 = new JButton("Niveau 1");
+        
 
         imagePane = new ImagePane();
 
@@ -45,4 +48,28 @@ public class IntGraphView extends JFrame {
             g.drawImage(model.getImage(),0,0,this);
         }
     }
+    
+    public void jouer() {
+    	jouer.setVisible(false);
+    	quitter.setVisible(false);
+    	titre.setVisible(false);
+    	affichemenu();
+    }
+    
+    public void affichemenu() {
+    	imagePane.removeAll();
+    	for (int i = 0; i < 5; i++) {
+    		imagePane.add(new JButton("i"));
+    	}
+    	JButton niv2 = new JButton("niveau 2");
+    	JButton niv3 = new JButton("niveau 3");
+    	JButton niv4 = new JButton("niveau 4");
+    	GridLayout gridlayout = new GridLayout(5,5);
+    	imagePane.setLayout(gridlayout);
+    	imagePane.add(niv1);
+    	imagePane.add(niv2);
+    	imagePane.add(niv3);
+    	imagePane.add(niv4);
+    }
+    
 }
